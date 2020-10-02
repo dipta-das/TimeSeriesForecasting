d0bccbe129b40586e1dee0bfef66b1cbdcc9905b
Dipta Das <dipta@promiti.com>
2020-10-02 18:25:05 +0600

    Time Series Forecast

---
+++ detailsofLibraries.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct  1 19:30:42 2020
+
+@author: Dipta Das
+"""
+
+import scipy as sc
+import numpy as np
+import pandas as pd
+import statsmodels as sm
+import sklearn as sk
+
+
+print('scipy: %s' % sc.__version__)
+print('Numpy: %s' % np.__version__)
+print('Matplotlib: %s' % pd.__version__)
+print('pandas: %s' % pd.__version__)
+print('statsmodels: %s' % sm.__version__)
+print('sklearn: %s' % sk.__version__)
